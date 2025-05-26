c38dbd1523cd53e1ceed993e8fe3218cbf2edb5c
Lakr <git@qaq.wiki>
2025-05-27 02:08:10 +0800

    Update MarkdownView

--- Frameworks/Storage/Package.swift
+++ Frameworks/Storage/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "Storage", targets: ["Storage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Lakr233/MarkdownView", from: "0.1.2"),
+        .package(url: "https://github.com/Lakr233/MarkdownView", from: "0.1.3"),
         .package(url: "https://github.com/Tencent/wcdb", from: "2.1.9"),
     ],
     targets: [
