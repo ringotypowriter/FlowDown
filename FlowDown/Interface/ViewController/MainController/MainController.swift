10d42c6f0703a3df24d70a2caef8535aac0cd15d
Zach <2801818616z@gmail.com>
2025-06-26 13:56:52 +0800

    fix: NewChat autofocus on mac

--- FlowDown/Interface/ViewController/MainController/MainController.swift
+++ FlowDown/Interface/ViewController/MainController/MainController.swift
@@ -421,7 +421,11 @@ class MainController: UIViewController {
 extension MainController: NewChatButton.Delegate {
     func newChatDidCreated(_ identifier: Conversation.ID) {
         sidebar.newChatDidCreated(identifier)
-        chatView.use(conversation: identifier)
+        chatView.use(conversation: identifier) { [weak self] in
+            #if targetEnvironment(macCatalyst)
+                self?.chatView.focusEditor()
+            #endif
+        }
     }
 }
 
