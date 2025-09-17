7af58707563ddb4889a0fd59632505f1e98ee398
Lakr <git@qaq.wiki>
2025-09-17 18:14:05 +0800

    Update ModelManager+Builtin.swift

--- FlowDown/Backend/Model/ModelManager+Builtin.swift
+++ FlowDown/Backend/Model/ModelManager+Builtin.swift
@@ -10,21 +10,11 @@ import Storage
 
 extension CloudModel {
     enum BuiltinModel: CaseIterable {
-        case gemini
         case mistral
         case qwen_coder
 
         var model: CloudModel {
             switch self {
-            case .gemini:
-                CloudModel(
-                    id: "d26f2641-2802-490c-afcd-5e053460f829",
-                    model_identifier: "gemini",
-                    endpoint: "https://text.pollinations.ai/openai/v1/chat/completions",
-                    context: .medium_64k,
-                    capabilities: [.tool, .visual],
-                    comment: String(localized: "This model is provided by pollinations.ai free of charge. Rate limit applies."),
-                )
             case .mistral:
                 CloudModel(
                     id: "78c9c492-ef7c-4504-aa95-04e4ce3a4602",
