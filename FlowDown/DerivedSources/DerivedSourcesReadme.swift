0e403ce1b51fb6228a22dea936add64b27b27785
Lakr <git@qaq.wiki>
2025-10-22 16:18:04 +0800

    Create DerivedSourcesReadme.swift

---
+++ FlowDown/DerivedSources/DerivedSourcesReadme.swift
@@ -0,0 +1,14 @@
+//
+//  DerivedSourcesReadme.swift
+//  FlowDown
+//
+//  Created by qaq on 22/10/2025.
+//
+
+import Foundation
+
+/*
+ 
+ Files are automatically generated. This file works like a .gitkeep file.
+ 
+ */
